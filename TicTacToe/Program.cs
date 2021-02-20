234cb6073e7144b7fab10485b4bc361afe3128a5
mohammedsouleymane <mohammedsouleymane@outlook.com>
2021-02-20 23:13:05 +0100

    update

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,41 +4,46 @@ namespace TicTacToe
 {
     class Program
     {
-        static int Winner(char[,] array)
+        static int Winner(char[] array)
         {
             int player = 0;
-            if (array[0, 0] == array[0, 1] && array[0, 1] == array[0, 2] && array[0, 2] == 'X')
+            if (array[0] == array[1] && array[1] == array[2] && array[2] == 'X')
                 player = 1;
-            else if (array[1, 0] == array[1, 0] && array[1, 0] == array[2, 0] && array[2, 0] == 'X')
+            else if (array[3] == array[4] && array[4] == array[5] && array[5] == 'X')
                 player = 1;
-            else if (array[2, 0] == array[2, 1] && array[2, 1] == array[2, 2] && array[2, 2] == 'X')
+            else if (array[6] == array[7] && array[7] == array[8] && array[8] == 'X')
                 player = 1;
-            else if (array[1, 2] == array[1, 2] && array[1, 2] == array[2, 2] && array[2, 2] == 'X')
+            else if (array[0] == array[3] && array[3] == array[6] && array[6] == 'X')
                 player = 1;
-            else if (array[0, 0] == array[1, 1] && array[1, 1] == array[2, 2] && array[2, 2] == 'X')
+            else if (array[1] == array[4] && array[4] == array[7] && array[7] == 'X')
                 player = 1;
-            else if (array[0, 2] == array[1, 1] && array[1, 1] == array[2, 0] && array[2, 0] == 'X')
+            else if (array[2] == array[5] && array[5] == array[8] && array[8] == 'X')
                 player = 1;
-            else if (array[0, 1] == array[1, 1] && array[1, 1] == array[2, 1] && array[2, 1] == 'X')
+            else if (array[0] == array[4] && array[4] == array[8] && array[8] == 'X')
+                player = 1;
+            else if (array[2] == array[4] && array[4] == array[6] && array[6] == 'X')
                 player = 1;
 
             //player 2
-            if (array[0, 0] == array[0, 1] && array[0, 1] == array[0, 2] && array[0, 2] == '0')
-                player = 1;
-            else if (array[1, 0] == array[1, 0] && array[1, 0] == array[2, 0] && array[2, 0] == '0')
-                player = 1;
-            else if (array[2, 0] == array[2, 1] && array[2, 1] == array[2, 2] && array[2, 2] == '0')
-                player = 1;
-            else if (array[1, 2] == array[1, 2] && array[1, 2] == array[2, 2] && array[2, 2] == '0')
-                player = 1;
-            else if (array[0, 0] == array[1, 1] && array[1, 1] == array[2, 2] && array[2, 2] == 'O')
-                player = 1;
-            else if (array[0, 2] == array[1, 1] && array[1, 1] == array[2, 0] && array[2, 0] == 'O')
-                player = 1;
-            else if (array[0, 1] == array[1, 1] && array[1, 1] == array[2, 1] && array[2, 1] == 'O')
+            if (array[0] == array[1] && array[1] == array[2] && array[2] == 'O')
                 player = 2;
+            else if (array[3] == array[4] && array[4] == array[5] && array[5] == 'O')
+                player = 2;
+            else if (array[6] == array[7] && array[7] == array[8] && array[8] == 'O')
+                player = 2;
+            else if (array[0] == array[3] && array[3] == array[6] && array[6] == 'O')
+                player = 2;
+            else if (array[1] == array[4] && array[4] == array[7] && array[7] == 'O')
+                player = 2;
+            else if (array[2] == array[5] && array[5] == array[8] && array[8] == 'O')
+                player = 2;
+            else if (array[0] == array[4] && array[4] == array[8] && array[8] == 'O')
+                player = 2;
+            else if (array[2] == array[4] && array[4] == array[6] && array[6] == 'O')
+                player = 2;
+                
 
-            return player;
+                    return player;
 
         }
         static void Main(string[] args)
@@ -46,26 +51,19 @@ namespace TicTacToe
 
             char rematch = 'j';
 
-
-
-
             do
             {
-                char[,] layout = {
-                            { '1', '2', '3' },
-                            { '4', '5', '6'},
-                             { '7', '8', '9'}
-                  };
+                char[] layout = {'1', '2', '3','4', '5', '6','7', '8', '9'};
                 Console.WriteLine($@" 
   __________________
  |                  |
- |   {layout[0, 0]}  |  {layout[0, 1]}  |  {layout[0, 2]}  |  
+ |   {layout[0]}  |  {layout[1]}  |  {layout[2]}  |  
  | _____|_____|_____|
  |      |     |     |
- |   {layout[1, 0]}  |  {layout[1, 1]}  |  {layout[1, 2]}  |    
+ |   {layout[3]}  |  {layout[4]}  |  {layout[5]}  |    
  | _____|_____|_____|
  |      |     |     |
- |   {layout[2, 0]}  |  {layout[2, 1]}  |  {layout[2, 2]}  |   
+ |   {layout[6]}  |  {layout[7]}  |  {layout[8]}  |   
  |__________________|");
 
 
@@ -89,34 +87,27 @@ namespace TicTacToe
                     }
 
                     Console.WriteLine($"Player {player} move");
-                    string position = Console.ReadLine();
-                    int posX = 0;
-                    int posY = 0;
-                    for (int j = 0; j < layout.GetLength(0); j++)
+                    char position = char.Parse(Console.ReadLine());
+                    int posX = -1;
+                    for (int j = 0; j < layout.Length; j++)
                     {
-                        for (int l = 0; l < layout.GetLength(1); l++)
-                        {
-                            if (layout[j, l] == char.Parse(position))
-                            {
-                                posX = j;
-                                posY = l;
-                            }
-                        }
+                        if (layout[j] == position)
+                            posX = j;
                     }
-
-                    layout[posX, posY] = sign;
+                    
+                    layout[posX] = sign;
 
                     Console.Clear();
                     Console.WriteLine($@" 
   __________________
  |                  |
- |   {layout[0, 0]}  |  {layout[0, 1]}  |  {layout[0, 2]}  |  
+ |   {layout[0]}  |  {layout[1]}  |  {layout[2]}  |  
  | _____|_____|_____|
  |      |     |     |
- |   {layout[1, 0]}  |  {layout[1, 1]}  |  {layout[1, 2]}  |    
+ |   {layout[3]}  |  {layout[4]}  |  {layout[5]}  |    
  | _____|_____|_____|
  |      |     |     |
- |   {layout[2, 0]}  |  {layout[2, 1]}  |  {layout[2, 2]}  |   
+ |   {layout[6]}  |  {layout[7]}  |  {layout[8]}  |   
  |__________________|");
 
                     if (Winner(layout) != 0)
